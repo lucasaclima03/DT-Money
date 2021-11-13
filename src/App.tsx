7293b9713a3cf67459952c2886a7902e4b3ac78d
Lucas Almeida <lucasaclima03@gmail.com>
2021-11-13 12:27:49 -0300

    ajustes nos components e adição de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,30 @@ import { GlobalStyle } from "./assets/styles/global";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement("#root");
 
 export function App() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
-    useState(false);
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true);
-  }
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false);
-  }
-  return (
-    <>
-      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
-      <Dashboard />
-      <NewTransactionModal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      />
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+        useState(false);
 
-      <GlobalStyle />
-    </>
-  );
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+    return (
+        <TransactionsProvider>
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+            <Dashboard />
+            <NewTransactionModal
+                isOpen={isNewTransactionModalOpen}
+                onRequestClose={handleCloseNewTransactionModal}
+            />
+
+            <GlobalStyle />
+        </TransactionsProvider>
+    );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,36 @@
+import React, { useContext } from "react";
 import { Container } from "./styles";
 import incomeImage from "../../assets/income.svg";
 import outcomeImage from "../../assets/outcome.svg";
 import totalImage from "../../assets/total.svg";
+import { TransactionsContext } from "../../TransactionsContext";
 
 export function Summary() {
-  return (
-    <Container>
-      <div>
-        <header>
-          <p>Entradas</p>
-          <img src={incomeImage} alt="Entradas" />
-        </header>
-        <strong>R$1.000,00</strong>
-      </div>
-      <div>
-        <header>
-          <p>Saídas</p>
-          <img src={outcomeImage} alt="Saídas" />
-        </header>
-        <strong>-R$500,00</strong>
-      </div>
-      <div className="highlight-background">
-        <header>
-          <p>Total</p>
-          <img src={totalImage} alt="Total" />
-        </header>
-        <strong>R$500,00</strong>
-      </div>
-    </Container>
-  );
+    const { transactions } = useContext(TransactionsContext);
+    console.log(transactions);
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImage} alt="Entradas" />
+                </header>
+                <strong>R$1.000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImage} alt="Saídas" />
+                </header>
+                <strong>-R$500,00</strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImage} alt="Total" />
+                </header>
+                <strong>R$500,00</strong>
+            </div>
+        </Container>
+    );
 }
